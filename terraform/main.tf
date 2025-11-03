3fbce0ae8c33ba07a27452f7596019197989a3fb
mafarhan72 <mafarhan1234@gmail.com>
2025-11-03 10:05:57 -0500

    making website public

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,43 @@
 resource "aws_s3_bucket" "name" {
   bucket = var.bucket_name
 }
+
+
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+  bucket = aws_s3_bucket.name.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site_access" {
+  bucket = aws_s3_bucket.name.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.name.bucket
+  policy = jsondecode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "Statement1",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : "${aws_s3_bucket.name.arn}/*"
+        }
+      ]
+    }
+  )
+
+}
