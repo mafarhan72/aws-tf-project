ba65dc588b326c02827e1489e148540b590a2e6d
mafarhan72 <mafarhan1234@gmail.com>
2025-11-03 09:55:58 -0500

    UPLOADING INDEX FILE TO S3 BUCKET 2nd try

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,20 @@
 terraform {
+  backend "s3" {
+    bucket         = "tf-resources-farhan123"
+    region         = "ca-central-1"
+    key            = "github-actions/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "tf-resources-gha-lock"
+
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~>6.19"
     }
   }
+
 }
 
 provider "aws" {
